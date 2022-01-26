4518962a3a6f0aa76c52998ee6f2a8444bd00a46
sarthakvijay <vijaysarthak0744@gmail.com>
2022-01-26 16:52:18 +0530

    initial commit

---
+++ hp/src/test/java/com/idea/hp/HpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea.hp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
